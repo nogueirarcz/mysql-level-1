092404b8ced136e16045002203b86dd96d903bca
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-09-16 08:08:53 -0300

    Adicionando categorias

--- aulas/aula-05.sql
+++ aulas/aula-05.sql
@@ -9,3 +9,11 @@ insert into tbl_editoras (nome_editora) values ('Wiley');
 
 -- Consultando os valores inseridos
 select * from tbl_editoras;
+
+-- Vamos adicionar categorias
+insert into tbl_categorias (id_categoria, nome_categoria)
+values (1, 'Tecnologia');
+values (2, 'História');
+values (3, 'Literatura');
+values (4, 'Astronomia');
+values (5, 'Botânica');
