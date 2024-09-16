a772fb31961775d253e307278c5eded1619f9114
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-09-16 07:30:09 -0300

    Adicionando novas constranints (foreign key) em livros

--- aulas/aula-03.sql
+++ aulas/aula-03.sql
@@ -17,3 +17,15 @@ alter table tbl_livro add column id_autor smallint not null;
 alter table tbl_livro add constraint fk_id_autor
 foreign key (id_autor)
 references tbl_autores (id_autor);
+
+-- Vamos aproveitar e insereir uma constraint de chave extrangeira para editora
+alter table tbl_livro add column id_editora smallint not null;
+
+alter table tbl_livro add constraint fk_id_editora
+foreign key (id_editora)
+references tbl_editoras (id_editora);
+
+-- Uma constraint para chave extrangeira da categoria
+alter table tbl_livro add constraint fk_id_categoria
+foreign key (id_categoria)
+references tbl_categorias (id_categoria);
