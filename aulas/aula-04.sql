dd6a197be509a512b2c155b2fa804c5aa237e27f
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-09-16 08:27:51 -0300

    Reorganizando os autores

--- aulas/aula-04.sql
+++ aulas/aula-04.sql
@@ -1,20 +1,21 @@
 -- Inserindo dados em uma tabela
 
 -- Vamos começar pela tabela de autores
-insert into tbl_autores (id_autor, nome_autor, sobrenome_autor) values
-(1, 'Daniel', 'Barret');
-
-insert into tbl_autores (id_autor, nome_autor, sobrenome_autor) values
-(2, 'Gerald', 'Carter');
-
-insert into tbl_autores (id_autor, nome_autor, sobrenome_autor) values
-(3, 'Mark', 'Sobel');
-
-insert into tbl_autores (id_autor, nome_autor, sobrenome_autor) values
-(4, 'William', 'Stanek');
-
-insert into tbl_autores (id_autor, nome_autor, sobrenome_autor) values
-(5, 'Richard', 'Blum');
+insert into tbl_autores values
+(1, 'Daniel', 'Barret'),
+(2, 'Gerald', 'Carter'),
+(3, 'Mark', 'Sobell'),
+(4, 'William', 'Stanek'),
+(5, 'Richard', 'Blum'),
+(6, 'Jostein', 'Gaarder'),
+(7, 'Umberto', 'Eco'),
+(8, 'Neil', 'De Grasse Tyson'),
+(9, 'Stephen', 'Hawking'),
+(10, 'Stephen', 'Jay Gould'),
+(11, 'Charles', 'Darwin'),
+(12, 'Alan', 'Turing'),
+(13, 'Simon', 'Monk'),
+(14, 'Paul', 'Scherz');
 
 -- Podemos consultar os resultados usando o comando select
 select * from tbl_autores;
--- aulas/aula-05.sql
+++ aulas/aula-05.sql
@@ -13,7 +13,15 @@ select * from tbl_editoras;
 -- Vamos adicionar categorias
 insert into tbl_categorias (id_categoria, nome_categoria)
 values (1, 'Tecnologia');
+
+insert into tbl_categorias (id_categoria, nome_categoria)
 values (2, 'História');
+
+insert into tbl_categorias (id_categoria, nome_categoria)
 values (3, 'Literatura');
+
+insert into tbl_categorias (id_categoria, nome_categoria)
 values (4, 'Astronomia');
+
+insert into tbl_categorias (id_categoria, nome_categoria)
 values (5, 'Botânica');
