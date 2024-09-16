d6236cf35b169b23b24aaed62f7f991fec844ba5
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-09-16 07:59:26 -0300

    Inserindo editoras

---
+++ aulas/aula-05.sql
@@ -0,0 +1,11 @@
+-- Inserindo dados na tablea de editoras
+insert into tbl_editoras (nome_editora) values ('Prentice Hall');
+
+insert into tbl_editoras (nome_editora) values ('O´ Reilly');
+
+insert into tbl_editoras (nome_editora) values ('Microsoft Press');
+
+insert into tbl_editoras (nome_editora) values ('Wiley');
+
+-- Consultando os valores inseridos
+select * from tbl_editoras;
